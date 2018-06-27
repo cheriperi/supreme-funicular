c1767b89b6cb905b0b7d50bfb749f85f0093c903
cherines <cheripe.caa@gmail.com>
2018-06-27 20:44:15 +0000

    Added the change of threshold

--- humSub.py
+++ humSub.py
@@ -1,18 +1,31 @@
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 
+threshold = 40
 
 def on_connect(client1, userdata, flags, rc):
 	print("Connected with result code" + str(rc))
 	
 	client.subscribe("/etsidi/hum")
+	client.subscribe("/etsidi/params")
 	
 def on_message(client1, userdata, message):
+	global threshold
 	print("message received ", str(message.payload.decode("utf-8")))
-	if int(message.payload) >= 40:
-		GPIO.output(LEDPIN, True)
-	else:
-		GPIO.output(LEDPIN, False)
+	if message.topic == "/etsidi/hum":
+		if int(message.payload) >= threshold:
+			GPIO.output(LEDPIN, True)
+			print("Value higher that threshold")
+		else:
+			GPIO.output(LEDPIN, False)
+	if message.topic == "/etsidi/params":
+		data = str(message.payload.decode("utf-8"))
+		dataStr = data.split("_")
+		dataNum = [int(num) for num in dataStr]
+		threshold = dataNum[0]
+		print("Changed threshold to " + str(threshold))
+		
+		
 
 LEDPIN = 27
 
--- tmpSub.py
+++ tmpSub.py
@@ -1,18 +1,30 @@
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 
+threshold = 30
 
 def on_connect(client1, userdata, flags, rc):
 	print("Connected with result code" + str(rc))
 	
 	client.subscribe("/etsidi/tmp")
+	client.subscribe("/etsidi/params")
 	
 def on_message(client1, userdata, message):
+	global threshold
 	print("message received ", str(message.payload.decode("utf-8")))
-	if int(message.payload) >= 30:
-		GPIO.output(LEDPIN, True)
-	else:
-		GPIO.output(LEDPIN, False)
+	if message.topic == "/etsidi/ldr":
+		if int(message.payload) >= threshold:
+			GPIO.output(LEDPIN, True)
+			print("Value higher that threshold")
+		else:
+			GPIO.output(LEDPIN, False)
+		
+	if message.topic == "/etsidi/params":
+		data = str(message.payload.decode("utf-8"))
+		dataStr = data.split("_")
+		dataNum = [int(num) for num in dataStr]
+		threshold = dataNum[2]
+		print("Changed threshold to " + str(threshold))
 
 LEDPIN = 17
 
